21af36dec15f7fb34d663e0ae835065400f60744
David Sullivan <>
2025-11-02 14:36:47 +0000

    Add demo of hidden input field and label.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import '/css/core.scss'
+
+let showBtn = document.getElementById('show-btn');
+
+showBtn.addEventListener('click', () => {
+    showBtn.innerText = showBtn.innerText === 'Show' ? 'Hide' : 'Show';
+    let hiddenContainer = document.getElementById('hidden-container');
+    hiddenContainer.classList.toggle('hidden');
+})
