762c0374ce255815ce0dafcc21adc8b2372abbbe
David Sullivan <>
2025-11-03 13:52:26 +0000

    Add demo for SVG image sizing. Also toast demo.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import '/css/core.scss'
 
 let showBtn = document.getElementById('show-btn');
 
-showBtn.addEventListener('click', () => {
-    showBtn.innerText = showBtn.innerText === 'Show' ? 'Hide' : 'Show';
-    let hiddenContainer = document.getElementById('hidden-container');
-    hiddenContainer.classList.toggle('hidden');
-})
+if (showBtn) {
+    showBtn.addEventListener('click', () => {
+        showBtn.innerText = showBtn.innerText === 'Show' ? 'Hide' : 'Show';
+        let hiddenContainer = document.getElementById('hidden-container');
+        hiddenContainer.classList.toggle('hidden');
+    })
+}
